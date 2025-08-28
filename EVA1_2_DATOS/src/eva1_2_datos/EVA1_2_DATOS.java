3a11c87cb274c90361029624184872a36ae1597c
diego-rogelio <diegorogelioph07@gmail.com>
2025-08-28 12:04:34 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //;significa fin de linea
+        //"   " Son cadenas de texto. Cuenta como un dato
+        System.out.println("Diego Perez");
+        //Numeros enteros
+        System.out.println(1000);
+        //Numeros decimales
+        System.out.println(500.3);
+        //Valores logicos (verdadero o falso)(true false)
+        System.out.println(true); //Se pone en azul porque es palabra reservada, (java la usa)
+        //Caracteres individuales
+        //Para println las dos comillas es lo mismo
+          System.out.println('c'); //'  ' significa caracter
+        System.out.println("c"); //"  " significa cadena de texto
+    }
+    
+}
