52aa0c2f9e18ac6c0e81af5f722136568252039b
diego-rogelio <diegorogelioph07@gmail.com>
2025-08-28 12:07:56 -0600

    Add files via upload

---
+++ EVA1_4_EMPLEO/src/eva1_4_empleo/EVA1_4_EMPLEO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_empleo;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_4_EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Si es "print" el texto se va hacia abajo, no salta de linea
+        //Si es "println" se imprime esa linea y la anterior si era "print" es como un "enter"
+        System.out.print("Empleo:");
+        System.out.println("Cocinero de cangreburguers");
+        System.out.print("Salario:");
+        System.out.println("2000 semanal");
+        System.out.print("Horas semana:");
+        System.out.println("35 horas");
+        System.out.print("Requiere ingles?");
+        System.out.println(true);
+        
+    }
+    
+}
