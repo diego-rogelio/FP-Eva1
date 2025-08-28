5bc61a699a9e91f803eddca86f3379d4e2b8f672
diego-rogelio <diegorogelioph07@gmail.com>
2025-08-28 12:07:08 -0600

    Add files via upload

---
+++ EVA1_3_DATOS/src/eva1_3_datos/EVA1_3_DATOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_datos;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_3_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Si es "print" el texto se va hacia abajo, no salta de linea
+        //Si es "println" se imprime esa linea y la anterior si era "print" es como un "enter"
+        System.out.print("Nombre del estudiante:");
+        System.out.println("Diego Perez");
+        System.out.print("Numero de control:");
+        System.out.println(25550290);
+        System.out.print("Edad:");
+        System.out.println(17);
+    }
+    
+    
+    
+}
