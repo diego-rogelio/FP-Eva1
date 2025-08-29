59e7d438b4f641c526a8f0db92d6da0555441ebe
diego-rogelio <diegorogelioph07@gmail.com>
2025-08-29 00:10:02 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Primero declarar variables
+        //Tipo de dato  Nombre del indentificador
+        String nombre;  //string es para cadenas de texto
+        int edad;    //int es para numeros enteros
+        double salario;  //double es para numeros reales medio grandes
+        //Inicializacion:
+        //identificador = valor
+        // = significa operador de asignacion
+        nombre="Diego Rogelio Perez Hernandez";
+        edad=17;
+        salario=23.5;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+               
+    }
+    
+}
