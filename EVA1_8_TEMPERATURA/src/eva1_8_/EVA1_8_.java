cb10088140292ef4605efe24fa85354826708263
diego-rogelio <diegorogelioph07@gmail.com>
2025-09-01 21:21:32 -0600

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_/EVA1_8_.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_8_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double celsius, fahrenheit;
+        fahrenheit= 48;
+        celsius= (fahrenheit-32)/1.8;
+        System.out.print("48 grados fahrenheit equivalen a: ");
+        System.out.print(celsius);
+        System.out.println(" grados centigrados");
+        
+        
+    }
+    
+}
