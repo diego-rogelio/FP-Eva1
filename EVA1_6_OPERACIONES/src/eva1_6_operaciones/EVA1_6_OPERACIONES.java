c548e975b4c355ba806bc191b51bd2896211b3a3
diego-rogelio <diegorogelioph07@gmail.com>
2025-09-01 21:18:55 -0600

    Add files via upload

---
+++ EVA1_6_OPERACIONES/src/eva1_6_operaciones/EVA1_6_OPERACIONES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_operaciones;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //para la mayoria de valores decimales se usa double, puedes poner enteros y decimales.
+        //se pueden declarar varias variables al mismo tiempo (solo sin son del mismo tipo), se tiene que separar por comas
+        double area, pi, radio;
+     pi=3.141592;
+     radio=5.42;
+     area=pi*(radio*radio);
+     System.out.print("El area de un circulo con un radio de 5.42, es: ");
+     System.out.println(area);
+     
+    }
+    
+}
